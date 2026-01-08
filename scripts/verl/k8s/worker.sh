ee06bf13c986aabc716a2fd5ede8ad2550c9cf1e
MikaStars39 <meloxdmy@qq.com>
2026-01-08 21:01:28 +0800

    [scripts] scripts

---
+++ scripts/verl/k8s/worker.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+export MASTER_REAL_IP="11.251.91.93"
+
+# 检查环境变量
+if [ -z "$MASTER_REAL_IP" ]; then
+    echo "❌ Error: Please set MASTER_IP environment variable."
+    echo "Usage: MASTER_IP=11.x.x.x ./worker_up.sh"
+    exit 1
+fi
+
+LOCAL_IP=$(hostname -I | awk '{print $1}')
+
+echo "👷 Joining Master at $MASTER_IP..."
+
+# 加入集群
+ray start \
+    --address "$MASTER_REAL_IP:6379" \
+    --node-ip-address "$LOCAL_IP" \
+    --num-gpus 8 \
+    --disable-usage-stats
+
+echo "✅ Worker has joined the cluster."
---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /mnt/llm-train/users/explore-train/qingyu/PeRL/verl
+pip3 install --no-deps --no-build-isolation -e .
+cd ..
